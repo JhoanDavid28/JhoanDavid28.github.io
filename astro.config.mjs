e3e56905920a1db43d6528404e93fb744a1cbbc4
JHOAN DAVID PILLAPA LLERENA <66537133+JhoanDavid28@users.noreply.github.com>
2025-07-06 21:09:39 -0500

    Actualización de configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jhoandavidpll.github.io',
+  site: 'https://JhoanDavid28.github.io',
   base: 'my-repo',
   devToolbar: {
     enabled: false
